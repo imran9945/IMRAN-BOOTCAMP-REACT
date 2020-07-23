ae46cbe005ea3c7a81055479d8ffa43548f22a19
imran9945 <62025696+imran9945@users.noreply.github.com>
2020-07-23 23:31:31 +0500

    Initial commit reactfirstapp

---
+++ freactapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      HELLO WORLD FROM FIRST REACT APP!!!
+    </div>
+  );
+}
+
+export default App;
